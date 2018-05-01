892fd169c2f9af11faf227361c69ae42743f098a
Christine Quan <christine@periscopedata.com>
2018-05-01 11:01:15 -0700

    COLORS BUT ACTUALLY

--- R/theme_periscope.R
+++ R/theme_periscope.R
@@ -14,7 +14,7 @@ theme_periscope <- function(
   color.background = palette[2],
   color.grid.major = palette[3],
   color.axis.text = palette[6],
-  color.axis.title = palette[7],
+  color.axis.title = palette[6],
   color.title = "#2F0035",
   color.subtitle = "#535368") 
   {
@@ -34,8 +34,6 @@ theme_periscope <- function(
     # Plot margins
     theme(plot.margin = grid::unit(c(0.35, 0.2, 0.3, 0.35), "cm")) +
     theme(text=element_text(size=base_size*9)) +
-    theme(plot.title=element_text(color=color.title, size=25, vjust=1.25)) +
-    scale_color_manual(values = periscope_colors) +
-    scale_fill_manual(values = periscope_colors)
-  )
+    theme(plot.title=element_text(color=color.title, size=25, vjust=1.25)) 
+  
 }
